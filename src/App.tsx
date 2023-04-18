2a6f083cd1319e6fbd16d397cf9b0c040f1b7f20
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-04-18 04:11:18 -0400

    feat: simple alert on each group item click

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London"];
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const [alertName, setAlertName] = useState("");
+
   const handleSelectItem = (item: string) => {
+    setAlertVisibility(false);
+
     console.log(item);
+
+    setTimeout(function () {
+      setAlertName(item);
+      setAlertVisibility(true);
+    }, 100);
   };
 
   return (
     <div>
+      {alertVisible && <Alert>{alertName}</Alert>}
       <ListGroup
         items={items}
         heading="Cities"
